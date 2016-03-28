54c4680348489cb3e5ec5f24cf70239fecaee2eb
ashfack <abdoulazid.ashfack@gmail.com>
2016-03-28 14:53:26 +0200

    Essai connexion via le cas

--- js/script_connexion.js
+++ js/script_connexion.js
@@ -21,3 +21,26 @@ $(document).ready( function () {
         return false;
     });
 });
+// $(document).ready( function () { 
+//     $("#Connect").submit( function() {  
+//         $.ajax(
+//         { 
+//            type: "POST", 
+//            url : "https://cas.univ-paris13.fr/cas/login?service=https%3A%2F%2Fent.univ-paris13.fr", 
+//            data: "username="+$("#pseudo").val()+"&password="+$("#mdp").val(), 
+//            success: function(msg)
+//            { 
+//                 if(msg==1) //Succes
+//                 {
+//                     window.location.replace("rechercher.php");
+//                 }
+//                 else // si la connexion en php n'a pas fonctionnée
+//                 {
+//                     if (msg!=0)
+//                         alert('Vous vous êtes trompés'); 
+//                 }
+//            }
+//         });
+//         return false;
+//     });
+// });
