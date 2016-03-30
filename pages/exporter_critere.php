b564847caf9fcfc7e208cbf025f2979a4391e833
Zgoda Rafal <zgoda.rafal@gmail.com>
2016-03-31 00:01:17 +0200

    jsTree

--- pages/exporter_critere.php
+++ pages/exporter_critere.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
    <head>
-		<title>Exporter avec critÃ¨res </title>
+		<title>Exporter avec critères </title>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../framework/jsTree/dist/themes/default/style.min.css" />
+
       <?php
          require('header_link.html');
          require('header_script.html');
@@ -23,8 +25,67 @@
 			            </br>
 			            <div class="col-md-4"> 
 
+  <div id="jstree">
+    <!-- in this example the tree is populated from inline HTML -->
+    <ul >
+      <li data-jstree='{"icon":"glyphicon glyphicon-folder-open"}'>Entreprise
+  <ul>
+    <li data-jstree='{"icon":"glyphicon glyphicon-file"}'>
+      Nom</li>
+    <li data-jstree='{"icon":"glyphicon glyphicon-leaf"}'>
+      Groupe</li>
+  </ul>
+</li>
+      <li data-jstree='{"icon":"glyphicon glyphicon-flash"}'>Contact
+        <ul>
+          <li id="child_node_1"> Nom    
+</li>
+          <li>Prénom</li>
+        </ul>
+          <li>Alternance</li>
+        </ul>
+      </li>
+    </ul>
+
+  </div>
+
+  <!-- 4 include the jQuery library -->
+<script src="../js/jquery.min.js"></script>
+  <!-- 5 include the minified jstree source -->
+  <script src="../framework/jsTree/dist/jstree.min.js"></script>
+
+  <script>
+  $(function () {
+    $('#jstree').jstree({  
+      "plugins" : [ "wholerow", "checkbox", "types" ]
+    });
+
+    $('#jstree').on("changed.jstree", function (e, data) {
+      console.log(data.selected);
+    });
+
+
+    $("#plugins1").jstree({
+      "checkbox" : {
+        "keep_selected_style" : false
+        }
+    });
+        $("#plugins7").jstree({
+    "types" : {
+      "default" : {
+        "icon" : "glyphicon glyphicon-flash"
+      },
+      "child_node_1" : {
+        "icon" : "glyphicon glyphicon-ok"
+      }
+    }
+  });
+  });
+
+  </script>
 
 <div class="checkbox" name="champsSelected" id="champsSelected">
+ 
   <h3>Champs a exporter
   </h3></br>
 </div>
