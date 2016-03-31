19adb486328d0372041a8cf930dd8727d0c218cf
ashfack <abdoulazid.ashfack@gmail.com>
2016-03-31 14:26:57 +0200

    modification mot de passe BDD

--- pages/db_connect.php
+++ pages/db_connect.php
@@ -4,10 +4,10 @@
 	//	session_start();
 		$servername ="mysql-srecreig.alwaysdata.net";
 		$username = "srecreig";
-		$password = "SREPARIS";
+		// $password = "SREPARIS";
 		$dbName = "srecreig_base";
 		//$conn = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
-		$conn = new PDO("mysql:host=$servername;dbname=srecreig_base", $username, $password);
+		$conn = new PDO("mysql:host=$servername;dbname=srecreig_base", $username,'205d49e7bd9b1272c09dcba47f856014');
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	catch(PDOException $e)
