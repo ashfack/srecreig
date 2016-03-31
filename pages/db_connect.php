900189c432ed5c901958c3c4def58890a2cfd4fc
ashfack <abdoulazid.ashfack@gmail.com>
2016-03-31 15:01:07 +0200

    modif de db_connect

--- pages/db_connect.php
+++ pages/db_connect.php
@@ -7,7 +7,7 @@
 		// $password = "SREPARIS";
 		$dbName = "srecreig_base";
 		//$conn = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
-		$conn = new PDO("mysql:host=$servername;dbname=srecreig_base", $username,md5('sre@paris13'));
+		$conn = new PDO("mysql:host=$servername;dbname=srecreig_base", $username,'sre@paris13');
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	catch(PDOException $e)
