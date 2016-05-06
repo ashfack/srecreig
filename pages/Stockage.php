4cfb7d7a1ecedc33c6c621cf44cac2c07e0a115c
ashfack <abdoulazid.ashfack@gmail.com>
2016-05-06 16:23:21 +0530

    Espace de stockage préfinal

--- pages/Stockage.php
+++ pages/Stockage.php
@@ -12,12 +12,16 @@
     <?php
     	require('header_script.html');
     ?>
+    <script src="../js/script_iframe.js"></script>
+
+
 </head>
 
 <body>
     <?php require('header.php'); ?>
-    <iframe src="http://srecreig.alwaysdata.net/srecreig/files/">
-      <p>Your browser does not support iframes.</p>
+    <iframe src="http://srecreig.alwaysdata.net/srecreig/files/" width="100%" height="500px" id="iframe1" marginheight="0" 
+        frameborder="0" onLoad="autoResize('iframe1');">
+      <p>Navigateur incompatible.</p>
     </iframe>	
 </body>
 </html>	
