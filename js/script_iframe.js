8d8abb7cad4ee003c87d007058a78f949c92c28c
ashfack <abdoulazid.ashfack@gmail.com>
2016-05-06 17:14:00 +0530

    Search engine

--- js/script_iframe.js
+++ js/script_iframe.js
@@ -11,56 +11,26 @@ function autoResize(id)
     document.getElementById(id).height = (newheight) + "px";
     document.getElementById(id).width = (newwidth) + "px";
 }
-
-/* Ce script a été réalisé par Mike Hall (MHall75819@aol.com) */
-var NS4 = (document.layers);
-var IE4 = (document.all);
-var win = window;
-var n = 0;
-function Rechercher(str) 
-{
-	var txt, i, found;
-	if (str == "")
-		return false;
-	if (NS4) 
-	{
-		if (!win.find(str))
-			while(win.find(str, false, true))
-				n++;
-		else
-			n++;
-		// Si le mot n'a pas été trouvé (Netscape)
-		if (n == 0)
-			alert("Pas trouvé !");
-	}
-	if (IE4) 
-	{
-		txt = win.document.body.createTextRange();
-		// Find the nth match from the top of the page.
-		for (i = 0; i <= n && (found = txt.findText(str)) != false; i++) 
-		{
-			txt.moveStart("character", 1);
-			txt.moveEnd("textedit");
-		}
-		if (found) 
-		{
-			txt.moveStart("character", -1);
-			txt.findText(str);
-			txt.select();
-			txt.scrollIntoView();
-			n++;
-		}
-		else 
-		{
-			if (n > 0) 
-			{
-				n = 0;
-				Rechercher(str);
-			}
-			// Si le mot n'a pas été trouvé (Explorer)
-			else
-				alert("Pas trouvé !");
-		}
-	}
-	return false;
-}
+var s = document.querySelector('input[type="search"]'),
+    p = document.querySelector('p'),
+    find = function()
+    {
+        var words = p.innerText.split(' '),
+            i = words.length,
+            word = '';
+        while(--i) 
+        {
+            word = words[i];
+            if((word.toLowerCase() == s.value.toLowerCase())||(word.toLowerCase() == s.value.toLowerCase()+".")||(word.toLowerCase() == s.value.toLowerCase()+",")||(word.toLowerCase() == s.value.toLowerCase()+";")||(word.toLowerCase() == s.value.toLowerCase()+":")||(word.toLowerCase() == s.value.toLowerCase()+"!")||(word.toLowerCase() == s.value.toLowerCase()+"?")||(word.toLowerCase() == s.value.toLowerCase()+"\'")||(word.toLowerCase() == s.value.toLowerCase()+"\'\'")||(word.toLowerCase() == s.value.toLowerCase()+"\"")||(word.toLowerCase() == s.value.toLowerCase()+"»"))
+            {
+                words[i] = '<span class="highlight">' + word + "</span>";
+            }
+            /*else
+            {
+            
+            }*/
+        }
+        p.innerHTML = words.join(' ');
+    };
+s.addEventListener('keydown', find , false);
+s.addEventListener('keyup', find , false);
--- pages/Stockage.php
+++ pages/Stockage.php
@@ -13,17 +13,18 @@
     	require('header_script.html');
     ?>
     <script src="../js/script_iframe.js"></script>
+    <script type="text/javascript" language="JavaScript" src="../js/find5.js"></script>
 
 
 </head>
 
 <body>
     <?php require('header.php'); ?>
-    
-    <form name="Recherche" onSubmit="return Rechercher(this.mot.value);">
-        <input name="mot" type="text" size=20 onChange="n = 0;">
+
+    <!-- <form name="Recherche" onSubmit="return Rechercher(this.mot.value);">
+        <input name="mot" type="search" size=20 >
         <input type="submit" value="Rechercher">
-    </form>
+    </form> -->
     <iframe src="http://srecreig.alwaysdata.net/srecreig/files/" width="100%" height="500px" id="iframe1" marginheight="0" 
         frameborder="0" onLoad="autoResize('iframe1');">
       <p>Navigateur incompatible.</p>
