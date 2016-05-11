a7c18a70b2627902d138ff15e82a42c8ebcac84a
ZgodaRafal <rafal-@hotmail.fr>
2016-05-11 03:35:21 +0200

    video et stockage

--- pages/Stockage.php
+++ pages/Stockage.php
@@ -25,20 +25,20 @@
 <body>
     <?php require('header.php'); ?>
     <div class="container">
-        <div class="col-md-3"></div>
-        <div class="col-md-6">
-            <h1 class="text-center">Joindre un fichier </h1><br>
-            <center>
+        <div class="col-md-3">        <input style="margin-top: 30px;" id="nom" name="mot" type="search" size=20>
 
-        <input id="nom" name="mot" type="search" size=20>
-        <input type="button" id="Rechercher" value="Rechercher">    <br>
+            <input type="button" id="Rechercher" value="Rechercher">    <br>
+</div>
+        <div class="col-md-6">
+            <center>
     
     
-    <iframe style ="margin-top:20px;" src="../    files/" width="100%" height="500px" id="iframe1" marginheight="0" 
+    <iframe style ="margin-top:20px;" src="../files/" width="100%" height="500px" id="iframe1" marginheight="0" 
         frameborder="0" onLoad="autoResize('iframe1');">
       <p>Navigateur incompatible.</p>
     </iframe>
     <!-- <script type="text/javascript" language="JavaScript" src="../js/find5.js"></script>	 -->
+
             </center>
         </div>
         <div class="col-md-3"></div>
